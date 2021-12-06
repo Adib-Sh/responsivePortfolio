77537af54fd5dd27e13879757bb55e2cd3d55f51
neoadib <neoadib@gmail.com>
2021-12-06 18:45:29 +0100

    media query added

--- dist/js/index.js
+++ dist/js/index.js
@@ -3,7 +3,9 @@ const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu')
 const menuNav = document.querySelector('.menu-nav')
 const menuBranding = document.querySelector('.menu-branding')
-const navItems = document.querySelectorAll('.nav-items')
+const navItems = document.querySelectorAll('.nav-item')
+const navItemsText = navItems.forEach(item => item.innerText)
+const pathname = window.location.pathname;
 
 // Set initial states of menu
 let showMenu = false;
@@ -16,7 +18,7 @@ function toggleMenu() {
         menu.classList.add('show');
         menuNav.classList.add('show');
         menuBranding.classList.add('show');
-        navItems.forEach(item => classList.add('show'))
+        navItems.forEach(item => item.classList.add('show'));
 
         // Set menu state
         showMenu = true;
@@ -25,9 +27,17 @@ function toggleMenu() {
         menu.classList.remove('show');
         menuNav.classList.remove('show');
         menuBranding.classList.remove('show');
-        navItems.forEach(item => classList.remove('show'))
+        navItems.forEach(item => item.classList.remove('show'))
 
         // Set menu state
         showMenu = false;
     }
+}
+
+var currentPage = location.href;
+var allA = document.getElementsByClassName('nav-link');
+for (var i = 0, len = allA.length; i < len; i++) {
+   if (allA[i].href == currentPage) {
+      allA[i].className = "current";
+   }
 }
